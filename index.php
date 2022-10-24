0f8120eef34e0ad0bd5754821a593baa8093c7b6
Galih Purnama <Gpurnama59@gmail.com>
2022-10-24 11:00:39 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello Word! <br>";
+
+$nama = "Galih";
+$umur = 21;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun."; 
+
+$namaAdik = "Galeh";
+$umuradik = 15;
+
+echo "nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umuradik tahun,<br>";
+
+$selisihUmur = $umur - $umuradik;
+echo "selisih usia saya dan adik saya adalah $selisihUmur tahun ";
+
+
+
+?>
