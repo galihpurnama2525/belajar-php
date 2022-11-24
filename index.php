190d3ad999419235ae512a270f8d80f26c84422d
Galih Purnama <Gpurnama59@gmail.com>
2022-11-24 09:04:11 +0700

    add crud create

--- index.php
+++ index.php
@@ -1,19 +1,63 @@
 <?php
+// 1. buat koneksi dengan MySQL
+$con = mysqli_connect("localhost","root","","fakultas");
 
-echo "Hello Word! <br>";
+// 2. cek konek dengan mySQL
+if(mysqli_connect_errno()) {
+    echo "koneksi gagal" . mysqli_connect_error();
+}else{
+    echo "koneksi berhasil";
+}
 
-$nama = "Galih";
-$umur = 21;
+// 3. membaca data dari table mysql
+$query = "SELECT * FROM mahasiwa";
 
-echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun."; 
+// 4 tampilkan data, dengan menjalankan sql query
+$result = mysqli_query($con,$query);
+if ($result){
+    // tampilkan data satu persatu
+    while($row = mysqli_fetch_assoc($result)){
+       $mahasiwa[] = $row;
+    }
+    mysqli_free_result($result);
+}
 
-$namaAdik = "Galeh";
-$umuradik = 15;
+// 5. tutup koneksi mysql
+ mysqli_close($con);
 
-echo "nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umuradik tahun,<br>";
 
-$selisihUmur = $umur - $umuradik;
-echo "selisih usia saya dan adik saya adalah $selisihUmur tahun ";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <table border="1" style="width:100%;">
+    <tr>
+        <th>NIM</th>
+        <th>nama</th>
+    <tr>
+    <?php foreach($mahasiwa as $value): ?>
+    <tr>
+        <td><?php echo $value["nim"]; ?></td>
+        <td><?php echo $value["nama"]; ?></td>
+    <tr>
+        <?php endforeach; ?>
+    <table>
+</body>
+</html>
+
+
+
+
+
+
 
 
 
